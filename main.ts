5d0cf05c6ff63aae13ca20913d14e3864da4cc6d
Chris Howard <chrish@341design.com.au>
2025-05-24 16:26:34 +1000

    Initial commit

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, MarkdownSubView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface SourceModeStylingSettings {
-	mySetting: string;
+	rawModeEnabled: boolean;
 }
 
 const DEFAULT_SETTINGS: SourceModeStylingSettings = {
-	mySetting: 'default'
+	rawModeEnabled: true
 }
 
 export default class SourceModeStyling extends Plugin {
@@ -16,66 +16,10 @@ export default class SourceModeStyling extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'SourceMode Styling Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('sourcemode-styling-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sourcemode-styling-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SourceModeStylingModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sourcemode-styling-editor-command',
-			name: 'Source mode Styling editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Source mode Styling Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sourcemode-styling-modal-complex',
-			name: 'Open Source Mode modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SourceModeStylingModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SourceModeStylingSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 
 		// === Add mode class logic ===
 		const updateBodyModeClass = () => {
@@ -90,9 +34,10 @@ export default class SourceModeStyling extends Plugin {
 
 			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 			if (view) {
-				const mode = view.getMode && view.getMode();
-				if (mode && ["source", "live", "preview"].includes(mode)) {
-					body.classList.add(`obsidian-mode-${mode}`);
+				console.log("Raw mode enable",this.settings.rawModeEnabled);
+				console.log(view.getState());
+				if (view.getState().source === true && view.getState().mode === "source") {
+						body.classList.add(`obsidian-mode-raw`);
 				}
 			}
 		};
@@ -148,13 +93,11 @@ class SourceModeStylingSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Enable raw source mode styling')
+			.addToggle(text => text
+				.setValue(this.plugin.settings.rawModeEnabled)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.rawModeEnabled = value;
 					await this.plugin.saveSettings();
 				}));
 	}
